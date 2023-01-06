20118ad447001b6d26c91edc48e660e8dabc871e
Alerran Victor <alerransilva13@gmail.com>
2023-01-06 17:13:39 -0300

    ER3

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -20,6 +20,7 @@ mensagem = ""
 
 usuarioLogado = ""
 
+/*
 onSubmit() {
   console.log(this.loginModel)
 
@@ -32,4 +33,37 @@ onSubmit() {
   }
   )
   }
+*/
+
+onSubmit(){
+  console.log(this.loginModel)
+
+  let erroEncontrado = 0;
+
+  const listPalavras: string[] = ["select ", "from ", "drop ", "or ", "having ", "group ", "insert ", "exec ",
+ "\"", "\'", "--", "#", "*", ";"]
+
+
+  listPalavras.forEach(palavra => {
+    console.log("palavra atual: ", palavra)
+
+    if (this.loginModel.email.toLowerCase().includes(palavra)){
+      console.log("Palavra encontrada: ", palavra)
+      this.mensagem = "Dados inválidos" + palavra;
+
+      erroEncontrado = 1;
+    }
+  })
+
+  if (erroEncontrado == 0){
+    this.loginService.login(this.loginModel).subscribe( (response) => {
+      this.usuarioLogado = response.body.user.nome
+      console.log(this.usuarioLogado)
+      this.router.navigateByUrl('')
+    },
+    (respostaErro)=>{
+      this.mensagem = respostaErro.error
+    })
+  }
+}
 }
